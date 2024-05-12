58f3764e054c84a70e9f545c6515c1492a4453d9
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-05-12 23:26:37 +0300

    Shop-Page

--- frontend/vite-project/src/pages/ShopPage.jsx
+++ frontend/vite-project/src/pages/ShopPage.jsx
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import Categories from "../components/Categories/Categories";
+import Products from "../components/Products/Products";
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
+
 const ShopPage = () => {
-  return <div>ShopPage</div>;
+  return (
+    <Fragment>
+      <Categories />
+      <Products />
+      <CampaignSingle />
+      <Products />
+    </Fragment>
+  );
 };
 
 export default ShopPage;
