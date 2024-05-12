b0465fe1d87a076bb3b0a63a9744d78fcae7e0c0
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-05-12 23:48:39 +0300

    fix

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,13 +1,11 @@
 import HomePage from "./pages/HomePage";
 import React from "react";
 import "./App.css";
-import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
     <React.Fragment>
       <HomePage />
-      <ShopPage />
     </React.Fragment>
   );
 }
