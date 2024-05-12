07ece976f8f5b67eb3e38b1310ad7e056d77efd0
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-05-12 23:00:07 +0300

    Single-Campaign

---
+++ frontend/vite-project/src/components/CampaignSingle/CampaignSingle.jsx
@@ -0,0 +1,21 @@
+import "./CampaignSingle.css";
+
+const CampaignSingle = () => {
+  return (
+    <section className="campaign-single">
+      <div className="container">
+        <div className="campaign-wrapper">
+          <h2>YENİ SEZON ZARARINA SATIŞLAR...</h2>
+          <strong>%40 İNDİRİM</strong>
+          <span></span>
+          <a href="#" className="btn btn-lg">
+            ALIŞVERİŞE BAŞLA
+            <i className="bi bi-arrow-right"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CampaignSingle;
