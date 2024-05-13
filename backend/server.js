508f9380766d981ba930f91bf139ba17c80c79d6
Süleyman Selim <suleymanpirinc6@gmail.com>
2024-05-13 22:50:34 +0300

    Create-MongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const port = 5000;
+
+const connect = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://yusel-e-commerce:ZBL8AFSOq03kzxpw@e-commerce.iajdzyy.mongodb.net/")
+        console.log("connected to mongoDb");
+    } catch (error) {
+        throw error;
+    }
+}
 app.get("/", (req, res) => {
     res.send("express.js");
 });
 
-app.get("/api", (req,res) => {
-  res.send("this is bu api route");
+app.get("/api", (req, res) => {
+    res.send("this is bu api route");
 })
 
-app.listen(5000, () => {
-  console.log("Sunucu ${5000} portunda çalışıyor");
+app.listen(port, () => {
+    connect();
+    console.log(`Sunucu ${port} portunda çalışıyor`);
 })
