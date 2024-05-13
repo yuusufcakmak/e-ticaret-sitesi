bb27f63c9a50ce391ffb5eb9a0757fd63479d268
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-05-13 08:32:53 +0300

    contact-page

--- frontend/vite-project/src/pages/ContactPage.jsx
+++ frontend/vite-project/src/pages/ContactPage.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+import Header from "../components/Layout/Header/Header";
+import Footer from "../components/Layout/Footer/Footer";
+import Policy from "../components/Layout/Policy/Policy";
+import Contact from "../components/Contact/Contact";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <Contact />
+      <Policy />
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default ContactPage;
