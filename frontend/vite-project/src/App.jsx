878208afcef07f2a90a1c376142d24b90c4e362a
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-05-21 06:40:24 +0300

    Update App.jsx

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,11 +3,13 @@ import HomePage from "./pages/HomePage";
 // import ShopPage from "./pages/ShopPage";
 import "./App.css";
 import { MdContactPage } from "react-icons/md";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <React.Fragment>
       <HomePage />
+      <AuthPage />
       <MdContactPage />
     </React.Fragment>
   );
