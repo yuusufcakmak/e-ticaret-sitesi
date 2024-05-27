d4d629d476fd778503d199a43c0f0776721bb620
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-05-27 16:45:47 +0300

    search-modal

---
+++ frontend/vite-project/src/components/Modals/Search/Search.jsx
@@ -0,0 +1,59 @@
+import "./Search.css";
+
+// eslint-disable-next-line react/prop-types
+const Search = ({ isSearchShow, setIsSearchShow }) => {
+  return (
+    <div className={`modal-search ${isSearchShow ? "show" : ""} `}>
+      <div className="modal-wrapper">
+        <h3 className="modal-title">Ürün ara</h3>
+        <p className="modal-text">
+          Aradığınız ürünleri görmek için yazmaya başlayın.
+        </p>
+        <form className="search-form">
+          <input type="text" placeholder="Ürün Ara..." />
+          <button>
+            <i className="bi bi-search"></i>
+          </button>
+        </form>
+        <div className="search-results">
+          <div className="search-heading">
+            <h3>Arama Sonuçları</h3>
+          </div>
+          <div className="results">
+            <a href="#" className="result-item">
+              <img
+                src="img/products/product1/1.png"
+                className="search-thumb"
+                alt=""
+              />
+              <div className="search-info">
+                <h4>Ceket</h4>
+                <span className="search-sku">BarkodNo: PD0016</span>
+                <span className="search-price">3500 TL</span>
+              </div>
+            </a>
+            <a href="#" className="result-item">
+              <img
+                src="img/products/product1/1.png"
+                className="search-thumb"
+                alt=""
+              />
+              <div className="search-info">
+                <h4>Ceket</h4>
+                <span className="search-sku">BarkodNo: PD0016</span>
+                <span className="search-price">3500 TL</span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <i
+          className="bi bi-x-circle"
+          id="close-search"
+          onClick={() => setIsSearchShow(false)}
+        ></i>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
