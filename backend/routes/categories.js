a129a7758ea9f361906bfa6a5408abc9674267c6
Süleyman Selim <suleymanpirinc6@gmail.com>
2024-05-27 22:16:20 +0300

    Create-Categories postman link

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
+router.post("/", async (req , res) => {
+  try {
+    const myData = req.body;
+
+    console.log(myData);
+    res.status(200).send("ok");
+  } catch (error) {
+    console.log(error)
+  }
+}
+)
+
 // Tüm kategorileri getir (read-all)
 router.get("/", async  (req,res) => {
     res.send("Kategoriler getirildi");
