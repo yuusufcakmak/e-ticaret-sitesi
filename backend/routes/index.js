78921fe4f67bdc0c77c79f73dfff470735b53a75
Süleyman Selim <suleymanpirinc6@gmail.com>
2024-05-30 14:03:40 +0300

    Create-newUser

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 //diğer rota dosyalarını içe aktarma 
 const productRoute = require("./products.js");
+const authRoute = require("./auth.js");
 const categoryRoute = require("./categories.js");
 
 // her rotayı ilgili yolda kullanımı
-router.use("/categories",categoryRoute);
-router.use("/products",productRoute);
+router.use("/categories", categoryRoute);
+router.use("/auth", authRoute);
+router.use("/products", productRoute);
 
-module.exports=router;
+module.exports = router;
 // postman kurulumu tamamlandı
