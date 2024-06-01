d6cc8ed7e2a019a2e345f389e06ba5dff3621403
Süleyman Selim <suleymanpirinc6@gmail.com>
2024-06-01 13:22:18 +0300

    Create-email-control

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const bcyrpt = require("bcryptjs");
 const User = require("../models/User.js");
 
 //Kullanıcı olusturma (create-register)
 router.post("/register", async (req, res) => {
     try {
         const { username, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: "Email adress is already registered" });
+        }
+        const hashedPassword = await bcyrpt.hash(password, 10);
+
         const newUser = await new User({
-            username, email, password
+            username,
+            email,
+            password: hashedPassword,
         })
         await newUser.save();
         res.status(201).json(newUser)
