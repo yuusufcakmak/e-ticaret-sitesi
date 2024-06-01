848bda4b52c30f163b113563a62219c139865fd8
Süleyman Selim <suleymanpirinc6@gmail.com>
2024-06-01 16:14:28 +0300

    Create-RewievSchema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
+const ReviewSchema = mongoose.Schema(
+    {
+        text: { type: String, required: true },
+        rating: { type: Number },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    }
+)
+
 const ProductSchema = mongoose.Schema(
     {
         name: { type: String, required: true },
-        img: { type: String, required: true },
+        img: [{ type: String, required: true }],
+        reviews: [ReviewSchema]
     },
     { timestamps: true }
 );
