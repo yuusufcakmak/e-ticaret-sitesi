aa73b8df585a238189771161fe622dde0f58a5d8
Süleyman Selim <suleymanpirinc6@gmail.com>
2024-06-01 19:26:43 +0300

    Update-Coupons.js

--- backend/routes/coupons.js
+++ backend/routes/coupons.js
@@ -5,6 +5,11 @@ const Coupon = require("../models/Coupon.js");
 // Yeni kupon oluşturma (create)
 router.post("/", async (req, res) => {
     try {
+        const { code } = req.body;
+        const existingCoupon = await Coupon.findOne({ code });
+        if (existingCoupon) {
+            return res.status(400).json({ error: "This coupon is already exists" });
+        }
 
         const newCoupon = new Coupon(req.body);
         await newCoupon.save();
@@ -28,21 +33,40 @@ router.get("/", async (req, res) => {
 
 });
 
-// belirli bir kupon getirme (read-single)
+// belirli bir kupon getirme (read-single by coupon ıd)
 router.get("/:couponId", async (req, res) => {
     try {
         const couponId = req.params.couponId;
 
-            const coupon = await Coupon.findById(couponId);
-            if (!coupon) {
-                return res.status(404).json({ error: "Coupon not found" });  
-            }
-            res.status(200).json(coupon);
-      
+        const coupon = await Coupon.findById(couponId);
+        if (!coupon) {
+            return res.status(404).json({ error: "Coupon not found" });
+        }
+        res.status(200).json(coupon);
+
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: "Server error." })
     }
 });
+
+// belirli bir kupon getirme (read-single by coupon code)
+router.get("/code/:couponCode", async (req, res) => {
+    try {
+        const couponCode = req.params.couponCode;
+
+        const coupon = await Coupon.findOne({ code: couponCode });
+        if (!coupon) {
+            return res.status(404).json({ error: "Coupon not found" });
+        }
+        const { discountPercent } = coupon;
+        res.status(200).json({ discountPercent });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Server error." })
+    }
+});
+
 
 module.exports = router;
