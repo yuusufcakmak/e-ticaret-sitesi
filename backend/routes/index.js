7e09296f934e7616d4b85e2a3ead929bc4d6c57d
Yusuf Çakmak <yusuf.cakmak.2000@gmail.com>
2024-06-02 01:58:48 +0300

    admin-routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-//diğer rota dosyalarını içe aktarma 
+//diğer rota dosyalarını içe aktarma
 const productRoute = require("./products.js");
 const authRoute = require("./auth.js");
 const categoryRoute = require("./categories.js");
 const couponRoute = require("./coupons.js");
+const userRoute = require("./users.js");
 
 // her rotayı ilgili yolda kullanımı
 router.use("/categories", categoryRoute);
 router.use("/auth", authRoute);
 router.use("/products", productRoute);
 router.use("/coupons", couponRoute);
+router.use("/users", userRoute);
+
 module.exports = router;
 // postman kurulumu tamamlandı
